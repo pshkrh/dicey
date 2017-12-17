9875815065fd8b07dba6a477927841df7a21a344
Pushkar Kurhekar <pshkrh@gmail.com>
2017-12-17 13:38:19 +0530

    Add Navigation Menu

--- app/src/main/java/com/pshkrh/dicey/MainActivity.java
+++ app/src/main/java/com/pshkrh/dicey/MainActivity.java
@@ -1,20 +1,47 @@
 package com.pshkrh.dicey;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mToolbar = (Toolbar) findViewById((R.id.nav_actionbar));
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         Button rollButton = (Button) findViewById(R.id.rollButton);
 
@@ -50,5 +77,35 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.onedice){
+            Toast.makeText(this, "This is the One Dice Option!",Toast.LENGTH_SHORT).show();
+        }
+
+        if(id == R.id.twodice){
+            Toast.makeText(this, "This is the Two Dice Option!",Toast.LENGTH_SHORT).show();
+        }
+
+        if(id == R.id.threedice){
+            Toast.makeText(this, "This is the Three Dice Option!",Toast.LENGTH_SHORT).show();
+        }
+
+        if(id == R.id.specialdice){
+            Toast.makeText(this, "This is the Special Dice Option!",Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
